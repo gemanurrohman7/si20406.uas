4c235b986aadb1233509ca2cdf48479edc3b8791
gemanurrohman7 <gemanurrohman7@gmail.com>
2021-06-30 20:09:34 +0700

    kontak

---
+++ app/src/main/java/com/proyek/kontak/DataModel.java
@@ -0,0 +1,32 @@
+package com.proyek.kontak;
+
+
+public class DataModel
+{
+    String name,nohp;
+    DataModel()
+    {
+
+    }
+
+    public DataModel(String name, String nohp) {
+        this.name = name;
+        this.nohp = nohp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNohp() {
+        return nohp;
+    }
+
+    public void setNohp(String nohp) {
+        this.nohp = nohp;
+    }
+}
